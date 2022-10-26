d5c1e1d8d06c1bf61f2f172cf1550ff871b9d66a
Kaileb Hammontree <kailebhammontree@Kailebs-MacBook-Pro.local>
2022-10-26 01:12:19 -0400

    index.js updated with required modules

---
+++ index.js
@@ -0,0 +1,7 @@
+// Run the application from this file
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
